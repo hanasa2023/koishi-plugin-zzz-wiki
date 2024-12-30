972a59d868da982b933e485f930cc8e612e26c8c
hanasaki <hanasakayui2022@gmail.com>
2024-12-31 01:29:11 +0800

    ♻️ Rfactor command

--- src/index.tsx
+++ src/index.tsx
@@ -9,19 +9,32 @@ declare module 'koishi' {
   interface Tables {
     zzz_wiki: ZZZWikiData
   }
+
+  interface Context {
+    zzzWiki: ZZZWIKIService
+  }
 }
 
 export const name = 'zzz-wiki'
 export const inject = {
-  require: ['puppeteer', 'database'],
+  required: ['database', 'puppeteer'],
 }
 
-export interface Config {}
+export interface Config {
+  apiUrl: string
+}
 
-export const Config: Schema<Config> = Schema.object({})
+// TODO: 更换API URL
+export const Config: Schema<Config> = Schema.object({
+  apiUrl: Schema.string()
+    .default('http://localhost:3000')
+    .description('API接口url'),
+})
 
 export async function apply(ctx: Context) {
   const baseUrl = 'https://miyabi-cdn.hanasaki.tech'
+
+  ctx.zzzWiki = new ZZZWIKIService(ctx)
 
   ctx.model.extend(
     'zzz_wiki',
@@ -35,48 +48,50 @@ export async function apply(ctx: Context) {
     {
       primary: 'contentId',
       autoInc: false,
-    }
+    },
   )
 
   ctx.on('ready', async () => {
     try {
-      const wiki = new ZZZWIKIService(ctx)
-      await wiki.upsertData()
+      await ctx.zzzWiki.upsertData()
     } catch (error) {
       ctx.logger.error('Database initialization error:', error)
     }
   })
 
-  // TODO: 创建数据库
-  //从本地加载翻译json，若本地没有则从网络加载
-
   ctx
-    .command('zzz图鉴 [type: string] 查看zzz图鉴')
-    .option('list', '-l 查看可用图鉴列表')
-    .option('update', '-u 更新插件数据')
+    .command('zzzWiki [name:string] 查看图鉴')
+    .option('list', '-l [value:string] 查看可用图鉴列表', { value: 'all' })
     .option('card', '-c <value:string> 查看图鉴卡片')
+    .option('update', '-u 更新插件数据')
     .action(async ({ options }, arg) => {
       if (arg) {
-        if (Translator.has(arg)) {
-          return (
-            <img
-              src={`${baseUrl}/images/${Translator.translate(arg)}/total.png`}
-            />
-          )
-        } else {
-          return '图鉴不存在'
-        }
+        const info = await ctx.zzzWiki.searchWikiDataByName(arg)
+        const url = `${ctx.config.apiUrl}/${Translator.translate(info[0].type)}/${info[0].contentId}`
+        console.info(url)
+        const img = await ctx.zzzWiki.getCardByUrl(url)
+        return img
       }
 
       if (options.list) {
-        const list = Object.keys(zhToEn)
-        return '可用图鉴列表：\n' + list.join('\n')
+        if (options.list === 'all') {
+          const iList = Object.keys(zhToEn)
+          return '可用图鉴列表：\n' + iList.join('\n')
+        } else {
+          if (Translator.has(arg)) {
+            return (
+              <img
+                src={`${baseUrl}/images/${Translator.translate(arg)}/total.png`}
+              />
+            )
+          } else {
+            return '图鉴不存在'
+          }
+        }
       }
 
       if (options.card) {
-        const info = await ctx.database.get('zzz_wiki', {
-          aliasName: options.card,
-        })
+        const info = await ctx.zzzWiki.searchWikiDataByName(options.card)
         if (!info.length) {
           return '图鉴卡片不存在'
         }
@@ -84,6 +99,10 @@ export async function apply(ctx: Context) {
           info[0].contentId
         }.png`
         return <img src={url} />
+      }
+
+      if (options.update) {
+        await ctx.zzzWiki.upsertData()
       }
     })
 }
